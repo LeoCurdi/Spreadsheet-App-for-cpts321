0efeee578e342edb0900147e6701900279902d8b
LeoCurdi <ldcurdi@icloud.com>
2024-02-26 13:58:22 -0800

    TDD - wrote the rest of the test cases and skeletal code, all tests failed.

--- Solution/Spreadsheet_Leonardo_Curdi/Form1.cs
+++ Solution/Spreadsheet_Leonardo_Curdi/Form1.cs
@@ -1,36 +1,41 @@
 // Copyright (c) Leonardo Curdi - 11704166. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using SpreadsheetEngine;
 using System.ComponentModel;
+using SpreadsheetEngine;
 
 namespace Spreadsheet_Leonardo_Curdi {
     /// <summary>
-    /// 
+    /// Inherits from Form class.
+    /// Provides functionality for the spreadsheet application using the SpreadsheetEngine DLL.
     /// </summary>
     public partial class Form1 : Form {
 
         /// <summary>
-        /// 
+        /// Number of rows in the spreadsheet.
         /// </summary>
-        static int rows = 50;
+        public static int Rows = 50;
 
         /// <summary>
-        /// 
+        /// Number of columns in the spreadsheet.
         /// </summary>
-        static int columns = 26;
+        public static int Columns = 26;
 
         /// <summary>
-        /// 
+        /// An instance of the Spreadsheet class from the SpreadSheetEngine DLL.
         /// </summary>
         private Spreadsheet spreadsheet;
 
-        // constructor for the UI class
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// Constructor for the UI class.
+        /// </summary>
         public Form1() {
             this.InitializeComponent();
             this.InitializeDataGrid();
 
-            this.spreadsheet = new Spreadsheet(rows, columns);
+            // initialize the spreadsheet and sub to the cell property changed event.
+            this.spreadsheet = new Spreadsheet(Rows, Columns);
             this.spreadsheet.CellPropertyChanged += this.Cell_PropertyChanged;
         }
 
@@ -38,19 +43,20 @@ namespace Spreadsheet_Leonardo_Curdi {
         /// Event handler for when a cell's value has changed.
         /// Updates the text of the cell in the DataGridView.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">This is the object that is triggering an event.</param>
+        /// <param name="e">The arguments associated with the event.</param>
         private void Cell_PropertyChanged(object sender, PropertyChangedEventArgs e) {
             // get the cell whos value has changed
             SpreadsheetCell cell = (SpreadsheetCell)sender;
+
             // update the value of the corresponding cell in the DataGridView
             this.cellGrid.Rows[cell.RowIndex].Cells[cell.ColumnIndex].Value = cell.Value;
         }
 
         /// <summary>
-        ///
+        /// Initializes the data grid in the GUI.
+        /// Generates all rows and columns and gives them header names.
         /// </summary>
-        /// <param name="cellGrid"></param>
         public void InitializeDataGrid() {
             // start by clearing the spreadsheet
             // we can access the data grid in the spreadsheet designer using its property name
@@ -59,23 +65,29 @@ namespace Spreadsheet_Leonardo_Curdi {
 
             // create n columns programatically
             int asciiOffset = 65;
-            for (int charAscii = asciiOffset; charAscii < asciiOffset + Form1.columns; charAscii++) { // use ascii value for capital letters A - Z
+            for (int charAscii = asciiOffset; charAscii < asciiOffset + Form1.Columns; charAscii++) { // use ascii value for capital letters A - Z
                 string columnName = ((char)charAscii).ToString(); // cast from ascii code value to char, then convert to a string, since Add() takes strings
                 this.cellGrid.Columns.Add(columnName, columnName); // use the Add() to add a column to the data grid
             }
 
             // create m rows
-            this.cellGrid.RowCount = Form1.rows;
+            this.cellGrid.RowCount = Form1.Rows;
+
             // name the m rows programatically
-            for (int row = 1; row <= Form1.rows; row++) {
-                this.cellGrid.Rows[row - 1].HeaderCell.Value = (row).ToString(); // set the value of the header cell
+            for (int row = 1; row <= Form1.Rows; row++) {
+                this.cellGrid.Rows[row - 1].HeaderCell.Value = row.ToString(); // set the value of the header cell
             }
         }
 
+        /// <summary>
+        /// An event handler for when the user enters text into a cell in the GUI.
+        /// </summary>
+        /// <param name="sender">This is the object that is triggering an event.</param>
+        /// <param name="e">The arguments associated with the event.</param>
         private void cellGrid_CellValueChanged(object sender, DataGridViewCellEventArgs e) {
-            if (e.ColumnIndex >= 0 && e.RowIndex >= 0) {
-                string enteredText = this.cellGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
-                this.spreadsheet.GetCell(e.RowIndex, e.ColumnIndex).Text = enteredText;
+            if (e.ColumnIndex >= 0 && e.RowIndex >= 0) { // for some reason you have to check if the indexes are not negative.
+                string enteredText = this.cellGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString(); // get the entered text from the user.
+                this.spreadsheet.GetCell(e.RowIndex, e.ColumnIndex).Text = enteredText; // update the text of the cell. (the text will be evaluated in the engine then bubbled back up to the listener in the form to update the value in the GUI)
             }
         }
     }
--- Solution/TestSpreadsheet/TestSpreadsheet.cs
+++ Solution/TestSpreadsheet/TestSpreadsheet.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Leonardo Curdi - 11704166. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-using SpreadsheetEngine;
 using System.Numerics;
 using NUnit.Framework;
+using SpreadsheetEngine;
 
 namespace SpreadsheetTests {
     /// <summary>
@@ -43,6 +43,34 @@ namespace SpreadsheetTests {
             Assert.AreEqual(
                 26,
                 this.testSheet.ColumnCount);
+        }
+
+        /// <summary>
+        /// Changes the text of a cell, then checks if the value was updated.
+        /// </summary>
+        [Test]
+        public void TestChangingCellText() {
+            // set the text of a cell.
+            this.testSheet.GetCell(1, 5).Text = "cohesion";
+            // check if the value matches.
+            Assert.AreEqual(
+                "cohesion",
+                this.testSheet.GetCell(1, 5).Value);
+        }
+
+        /// <summary>
+        /// Changes the text of a cell to an equation, checks if the equation was evaluated properly.
+        /// </summary>
+        [Test]
+        public void TestEnteringEquation() {
+            // set the text of cell A1
+            this.testSheet.GetCell(0, 0).Text = "cohesion";
+            // set cell B2 equal to the value of cell A1
+            this.testSheet.GetCell(1, 1).Text = "=A1";
+            // check if the value of B2 was correctly evaluated to equal the contents of A1.
+            Assert.AreEqual(
+                "cohesion",
+                this.testSheet.GetCell(1, 1).Value);
         }
     }
 }
