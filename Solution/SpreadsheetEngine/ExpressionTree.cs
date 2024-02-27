5d9e79948754d19f1e764494320891b983edd9f3
LeoCurdi <ldcurdi@icloud.com>
2024-02-27 10:05:03 -0800

    Created branch for HW5, wrote skeletal code for ExpressionTree.

---
+++ Solution/SpreadsheetEngine/ExpressionTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetEngine {
+    public class ExpressionTree {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionTree"/> class.
+        /// Constructs the tree from an expression.
+        /// </summary>
+        public ExpressionTree() {
+            
+        }
+
+        /// <summary>
+        /// Sets the specified variable within the ExpressionTree variables dictionary.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="variableValue"></param>
+        public void SetVariable(string variableName, double variableValue) {
+
+        }
+
+        /// <summary>
+        /// Evaluates the expression to a double value.
+        /// </summary>
+        /// <returns></returns>
+        public double Evaluate() {
+            return 0;
+        }
+    }
+}
