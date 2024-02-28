ab9a1e7b2deefa0631f79b44dc261d1013ff6ed3
LeoCurdi <ldcurdi@icloud.com>
2024-02-27 19:14:45 -0800

    Implemented evaluate().

--- Solution/SpreadsheetEngine/ExpressionTree.cs
+++ Solution/SpreadsheetEngine/ExpressionTree.cs
@@ -5,8 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SpreadsheetEngine {
-    public class ExpressionTree {
 
+    /// <summary>
+    /// A class that takes in a string representation of an expression and evaluates it to a numeric value.
+    /// </summary>
+    public class ExpressionTree {
         /// <summary>
         /// A list of chars for the supported operators.
         /// </summary>
@@ -22,6 +25,10 @@ namespace SpreadsheetEngine {
         /// </summary>
         private ExpressionTreeNode rootNode;
 
+        /// <summary>
+        /// A lookup table that stores the corresponding numeric value for every variable.
+        /// </summary>
+        private Dictionary<string, double> variables = new Dictionary<string, double>();
 
 
         /// <summary>
@@ -35,8 +42,8 @@ namespace SpreadsheetEngine {
         /// <summary>
         /// Sets the specified variable within the ExpressionTree variables dictionary.
         /// </summary>
-        /// <param name="variableName"></param>
-        /// <param name="variableValue"></param>
+        /// <param name="variableName">The name of the target variable.</param>
+        /// <param name="variableValue">The value to set the variable to.</param>
         public void SetVariable(string variableName, double variableValue) {
 
         }
@@ -46,6 +53,11 @@ namespace SpreadsheetEngine {
         /// </summary>
         /// <returns>A double contianing the result of evaluating the tree.</returns>
         public double Evaluate() {
+            // make sure root is not null
+            if (this.rootNode != null) {
+                return this.rootNode.Evaluate();
+            }
+
             return -1;
         }
     }
